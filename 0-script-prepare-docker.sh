ed903692bc981b8bae599af5dcad490dcb16fc68
Tanatpat Khunchai <tanatpat.khu@ascendcorp.com>
2023-07-14 21:22:29 +0700

    update

--- 0-script-prepare-docker.sh
+++ 0-script-prepare-docker.sh
@@ -5,7 +5,7 @@ apt install -y apt-transport-https ca-certificates curl software-properties-comm
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 #apt-cache policy docker-ce
-apt install docker-ce -y
+apt install docker-ce net-tools -y
 
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose

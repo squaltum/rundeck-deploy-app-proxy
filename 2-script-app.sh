95de5341617bf86bf692d7a20fd29af29ae625b8
Tanatpat Khunchai <tanatpat.khu@ascendcorp.com>
2023-07-14 21:48:02 +0700

    udpate

--- 2-script-app.sh
+++ 2-script-app.sh
@@ -2,7 +2,7 @@
 
 useradd robot-automation
 mkdir -p /home/robot-automation/.ssh/
-cat files/rundeck/public-key /home/robot-automation/.ssh/authorized_keys
+cat files/rundeck/public-key > /home/robot-automation/.ssh/authorized_keys
 chmod 400 /home/robot-automation/.ssh/authorized_keys
 chown -R robot-automation:robot-automation /home/robot-automation/.ssh
 
@@ -11,7 +11,7 @@ cp -r files/docker-apps/nginx /opt/
 while read line
 do
 	DOCKERVAR=$( echo ${line} |gawk -F"=" '{ print $1 }' )
-	if [ ${DOCKERVAR} = "DOCKERURL" ];
+	if [ "${DOCKERVAR}" = "DOCKERURL" ];
 	then
 		DOCKERVAR=$( echo ${line} |gawk -F"=" '{ print $2 }' )
 		sed -i "s/DOCKERURL/${DOCKERVAR}/g" /opt/nginx/config/nginx.conf
